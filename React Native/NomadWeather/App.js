bb7100967f37c42826e0a7f5aa303e8b73750a2c
KIMSEOKJIN <102217654+SEOKKAMONI@users.noreply.github.com>
2022-07-27 02:07:27 +0900

    Icons Update

--- React Native/NomadWeather/App.js
+++ React Native/NomadWeather/App.js
@@ -1,14 +1,26 @@
 import react, { useEffect, useState } from 'react';
 import * as Location from "expo-location"
 import { View, StyleSheet, Text } from 'react-native';
-import { ScrollView, Dimensions, ActivityIndicator} from 'react-native-web';
+import { ScrollView, Dimensions, ActivityIndicator } from 'react-native-web';
+import { Fontisto } from '@expo/vector-icons'
 
 // AIzaSyDm7Acp7tm_hfWjOTwUw33abNMaprgxcGU
 
-const { width:SCREEN_WIDTH } = Dimensions.get("window"); 
+const { width: SCREEN_WIDTH } = Dimensions.get("window");
 // 사용자가 쓰는 디바이스 가로의 길이를 가져와줌
 
 const API_KEY = "38b048ff72f8696e679fd95ee17259f7";
+
+const icons = {
+  Clouds: "cloudy",
+  Clear: "day-sunny",
+  Atmosphere: "cloudy-gusts",
+  Snow: "snow",
+  Rain: "rains",
+  Drizzle: "rain",
+  Thunderstorm: "lightning",
+}
+
 
 export default function App() {
   // view 는 기본적으로 Flex Container 
@@ -19,15 +31,15 @@ export default function App() {
   const [city, setCity] = useState("Loading...");
   const [days, setDays] = useState([]);
   const [ok, setOk] = useState(true);
-  const getWeather = async() => {
-    const {granted} = await Location.requestForegroundPermissionsAsync();
-    if(!granted) {
+  const getWeather = async () => {
+    const { granted } = await Location.requestForegroundPermissionsAsync();
+    if (!granted) {
       setOk(false);
     }
-    const {coords: {latitude, longitude}} = await Location.getCurrentPositionAsync({accuracy:5});
-    const location = await Location.reverseGeocodeAsync({latitude, longitude}, {useGoogleMaps:false})
+    const { coords: { latitude, longitude } } = await Location.getCurrentPositionAsync({ accuracy: 5 });
+    const location = await Location.reverseGeocodeAsync({ latitude, longitude }, { useGoogleMaps: false })
     setCity(location[0].city);
-    fetch(`https://api.openweathermap.org/data/2.5/onecall?lat=${latitude}&lon=${longitude}&exclude=alerts&appid=${API_KEY}&units=metric`);
+    const response = fetch(`https://api.openweathermap.org/data/2.5/onecall?lat=${latitude}&lon=${longitude}&exclude=alerts&appid=${API_KEY}&units=metric`);
     const json = await response.json();
     setDays(json.daily)
   }
@@ -41,33 +53,42 @@ export default function App() {
       <View style={styles.city}>
         <Text style={styles.cityName}>{city}</Text>
       </View>
-      <ScrollView 
+      <ScrollView
         pagingEnabled // 가로 스크롤을 좀더 끈끈하게 해주는 props
         horizontal // 스크롤뷰를 가로로 설정하는 props
         contentContainerStyle={styles.weather}
-        showsVerticalScrollIndicator = {false} // 스크롤시 생기는 스크롤바를 없애주는 props
+        showsVerticalScrollIndicator={false} // 스크롤시 생기는 스크롤바를 없애주는 props
       >
 
         {days.length === 0 ? (
-        <View style={styles.day}>
-          <ActivityIndicator 
-            color="white" 
-            size="large"
-            style={{ marginTop:10 }}
-          />
-        </View>
-        )  :  (
-          days.map((day,index) => {
+          <View style={styles.day}>
+            <ActivityIndicator
+              color="white"
+              size="large"
+              style={{ marginTop: 10 }}
+            />
+          </View>
+        ) : (
+          days.map((day, index) => {
             <View key={index} style={styles.day}>
-              <Text style={styles.temp}>
-                {parseFloat(day.temp.day).toFixed(1)}
-              </Text>
+              <View style={{
+                  flexDirection: "row",
+                  alignItems: "center",
+                  width: "100%",
+                  justifyContent: "space-between"
+                  }}>
+                <Text style={styles.temp}>
+                  {parseFloat(day.temp.day).toFixed(1)}
+                </Text>
+                <Fontisto name={icons[day.weather[0].main]} size={68} color="white" style={{marginRight: 50}} />
+              </View>
+
               <Text style={styles.description}>{day.weather[0].main}</Text>
               <Text style={styles.tinyText}>{day.weather[0].description}</Text>
             </View>
           })
         )}
-        
+
       </ScrollView>
     </View>
   );
@@ -75,7 +96,7 @@ export default function App() {
 
 const styles = StyleSheet.create({
   container: {
-    flex: 1, 
+    flex: 1,
     backgroundColor: "tomato",
   },
   city: {
@@ -86,7 +107,7 @@ const styles = StyleSheet.create({
   cityName: {
     fontSize: 58,
     fontWeight: "500"
-  },  
+  },
   weather: {
   },
   day: {
