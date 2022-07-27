c27c0fae6c29c392da46cb60527086b6753ca723
KIMSEOKJIN <102217654+SEOKKAMONI@users.noreply.github.com>
2022-07-27 13:21:11 +0900

    Persist

--- React Native/WorkHardTravelHardApp/App.js
+++ React Native/WorkHardTravelHardApp/App.js
@@ -1,25 +1,39 @@
 import { data } from 'cheerio/lib/api/attributes';
 import { StatusBar } from 'expo-status-bar';
-import { useState } from 'react';
+import { useEffect, useState } from 'react';
 import { StyleSheet, Text, View, TouchableOpacity, Pressable } from 'react-native';
 import { ScrollView, TextInput, TouchableHighlight, TouchableWithoutFeedback } from 'react-native-web';
 import { theme } from "./colors"
+import AsnyncStorage from "@react-native-async-storage/async-storage"
+
+const STORAGE_KEY = "@toDos"
 
 export default function App() {
   const [working, setWorking] = useState(true);
   const [text, setText] = useState("");
   const [toDos, setToDos] = useState({});
+  useEffect(() => {
+    loadToDos();
+  }, []); 
   const travel = () => setWorking(false);
   const work = () => setWorking(true);
   const onChangeText = (payload) => {
     setText(payload)
   }
-  const addToDO = () => {
+  const saveToDos = async (toSave) => {
+    await AsnyncStorage.setItem(STORAGE_KEY, JSON.stringify(toSave))
+  }
+  const loadToDos = async () => {
+    const s =  await AsnyncStorage.getItem(STORAGE_KEY)
+    setToDos(JSON.parse(s))
+  }
+  const addToDO = async () => {
     if(text === "") {
       return;
     }
-    const newToDos = {...toDos, [Date.now()]: {text, work:working}}
-    setToDos(newToDos)
+    const newToDos = {...toDos, [Date.now()]: {text, working}}
+    setToDos(newToDos);
+    await saveToDos(newToDos);
     setText("")
   }
   console.log(toDos)
@@ -46,9 +60,10 @@ export default function App() {
       <ScrollView>
         {
           Object.keys(toDos).map(key => (
+            toDos[key].working === working ?
             <View style={styles.toDo} key={key}>
               <Text style={styles.toDoText}>{toDos[key].text}</Text>
-            </View>
+            </View> : null
           ))
         }
       </ScrollView>
